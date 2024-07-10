76450dc9f01d7631833fe7211baaa7fd43719cd4
sonal <saumdas@localhost>
2024-07-10 17:53:36 -0400

    Created Project structure

---
+++ src/main/java/recipeScraping/App.java
@@ -0,0 +1,13 @@
+package recipeScraping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
