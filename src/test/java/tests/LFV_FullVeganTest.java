c81376d034e3714e346cca4f86dd334ffc7b42dd
reshma <aarnaipatil@gmail.com>
2024-07-12 13:55:43 -0500

    Commiting changes

--- src/test/java/tests/LFV_FullVeganTest.java
+++ src/test/java/tests/LFV_FullVeganTest.java
@@ -1,17 +1,19 @@
 package tests;
-
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import logger.Log;
 
 public class LFV_FullVeganTest extends BaseTest{
-
 	
 	@Test
-	public void extractRecipe()
+	public void extractRecipe() throws InterruptedException
 	{
 		Log.info("I am inside extractRecipe method");	
+
+		
+		
+		
 		
 	}
 	
